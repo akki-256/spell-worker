cac20a1a20a10ee4c29115c90e15adcfae130602
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2025-03-21 16:19:20 +0900

    add:加速度と魔法の判定作成

--- nitro/server/routes/spell.ts
+++ nitro/server/routes/spell.ts
@@ -1,17 +1,22 @@
 import { spellChecker } from "~/utils/spellChecker";
+import { getIsMoving } from "./stick.post";
 import { SpellMessage } from "~/types";
 
-// WebSocket: 呪文と音声テキストの取得と魔法判定の返却 (ws: spell)
+// WebSocket: 呪文と音声テキストの取得と魔法判定の返却 (ws: /spell)
 export default defineWebSocketHandler({
   open(peer) {
     peer.send({ user: "server", message: "open" });
   },
   message: async (peer, message) => {
-    const data: SpellMessage = await message.json();
-    const { userSpell, correctSpell } = data;
+    const spell: SpellMessage = await message.json();
+
+    const isMoving = getIsMoving();
+    const isSpellMatched = spellChecker(spell.userSpell, spell.correctSpell);
+    const isMagicSuccess = isMoving && isSpellMatched;
+
     peer.send({
       user: "server",
-      message: spellChecker(userSpell, correctSpell),
+      message: isMagicSuccess,
     });
   },
   close(peer) {
--- nitro/server/types/index.ts
+++ nitro/server/types/index.ts
@@ -2,3 +2,11 @@ export type SpellMessage = {
   userSpell: string;
   correctSpell: string;
 };
+
+export type SensorData = {
+  accel_x: number;
+  accel_y: number;
+  accel_z: number;
+  angle_x: number;
+  angle_y: number;
+};
