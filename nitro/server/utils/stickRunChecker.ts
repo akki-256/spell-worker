d8e29678ba9827d323ccf3258e42e702f73110bd
sirufu <miihon518@gmail.com>
2025-03-27 19:08:40 +0900

    加速度判定を実装してみた

--- nitro/server/utils/stickRunChecker.ts
+++ nitro/server/utils/stickRunChecker.ts
@@ -1,6 +1,16 @@
 import { SensorData } from "~/types";
 
 //  杖の振り判定処理
-export const stickRunChecker = (sensorData: SensorData): boolean => {
-  return sensorData.accel_x < -0.02 ? true : false; // 仮の判定
+export const stickRunChecker = (sensorData: SensorData,previousSensorData): boolean => {
+  
+  // console.log(previousSensorData.accel_x,sensorData.accel_x);
+  let check = false;
+
+  // 
+  if (Math.abs(0.2) < sensorData.accel_x - previousSensorData.accel_x || Math.abs(0.2) < sensorData.accel_y - previousSensorData.accel_y || Math.abs(0.2) < sensorData.accel_z - previousSensorData.accel_z){
+    check = true;
+  } else {
+    check = false;
+  }
+  return check; // 仮の判定
 };
