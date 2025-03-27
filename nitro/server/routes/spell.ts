aa3b1a5707888acc74fbd3785fbaed7b8fa49e34
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2025-03-27 14:47:31 +0900

    add:fastApi連携とフロント用のバリデーションチェック

--- nitro/server/routes/spell.ts
+++ nitro/server/routes/spell.ts
@@ -1,7 +1,8 @@
 import { spellChecker } from "~/utils/spellChecker";
 import { getIsMoving } from "./stick.post";
-import { CorrectSpell } from "~/types";
+import { ConvertedUserSpell, CorrectSpell } from "~/types";
 import { getCorrectSpell } from "./setup";
+import { sendPost } from "~/utils/sendPost";
 
 // WebSocket: 呪文と音声テキストの取得と魔法判定の返却 (ws: /spell)
 export default defineWebSocketHandler({
@@ -9,18 +10,39 @@ export default defineWebSocketHandler({
     peer.send({ user: "server", message: "open" });
   },
   message: async (peer, message) => {
-    const userSpell: string = await message.json();
-    const correctSpell: CorrectSpell = getCorrectSpell();
-    const isMoving: boolean = getIsMoving();
+    try {
+      // メッセージが空でないかチェック
+      if (!message) {
+        peer.send({ user: "server", message: "Error: empty message received" });
+        return;
+      }
+      // 音声テキストの取得
+      const userSpell: string = await message.json();
 
-    const magicSuccess = isMoving
-      ? spellChecker(userSpell, correctSpell.jsontype)
-      : null;
+      // 音声テキストのカタカナ変換
+      const convertedUserSpell: ConvertedUserSpell = await sendPost(
+        "/process",
+        userSpell
+      );
 
-    peer.send({
-      user: "server",
-      message: { magicSuccess: magicSuccess, isMoving: isMoving },
-    });
+      // 正解呪文の取得
+      const correctSpell: CorrectSpell = getCorrectSpell();
+
+      // 杖の振り判定の取得
+      const isMoving: boolean = getIsMoving();
+
+      // 魔法の判定
+      const magicSuccess = isMoving
+        ? spellChecker(convertedUserSpell.message, correctSpell.jsontype)
+        : null;
+
+      peer.send({
+        user: "server",
+        message: { magicSuccess: magicSuccess, isMoving: isMoving },
+      });
+    } catch (error) {
+      peer.send({ user: "server", message: `Error: ${error.message}` });
+    }
   },
   close(peer) {
     peer.send({ user: "server", message: "close" });
