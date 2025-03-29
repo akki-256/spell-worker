13fa2aebf05cb4e9ebd77d51859a3cbdd474489f
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2025-03-29 15:41:04 +0900

    update:/handleをsse通信に変更

--- nitro/server/routes/handle.ts
+++ nitro/server/routes/handle.ts
@@ -1,24 +1,20 @@
 import { getIsMoving } from "./stick.post";
 
 // WebSocket: 呪文と音声テキストの取得と魔法判定の返却 (ws: /spell)
-export default defineWebSocketHandler({
-    open(peer) {
-        peer.send({ user: "server", message: "open" });
-    },
-    message: async (peer) => {
-        try {
-            // 杖の振り判定の取得
-            const isMoving: boolean = getIsMoving();
+export default defineEventHandler(async (event) => {
+  const eventStream = createEventStream(event);
 
-            peer.send({
-                user: "server",
-                message: { isMoving: isMoving },
-            });
-        } catch (error) {
-            peer.send({ user: "server", message: `Error: ${error.message}` });
-        }
-    },
-    close(peer) {
-        peer.send({ user: "server", message: "close" });
-    },
+  // 杖の振り判定の取得
+  const isMoving: boolean = getIsMoving();
+
+  const interval = setInterval(async () => {
+    await eventStream.push(`${isMoving}`);
+  }, 100);
+
+  eventStream.onClosed(async () => {
+    clearInterval(interval);
+    await eventStream.close();
+  });
+
+  return eventStream.send();
 });
