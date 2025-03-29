8526262839019176a589025cd0b6f1ca7d40ddb3
ayu <umaminokiami@gmail.com>
2025-03-29 18:03:32 +0900

    fix: sse

--- nitro/server/routes/handle.ts
+++ nitro/server/routes/handle.ts
@@ -1,26 +1,35 @@
-import { getIsMoving } from "./stick.post";
-import { EventEmitter } from "events";
+import { getIsMoving } from './stick.post';
+import { eventEmitter } from '~/utils/sendStick';
 
 // 加速度が変わるたびにフロントにsseで送信
 export default defineEventHandler(async (event) => {
   const eventStream = createEventStream(event);
-  const eventEmitter = new EventEmitter();
 
-  const push = (isMoving: boolean) => {
-    eventStream.push(`${isMoving}`);
-  };
+  console.log('stickRun start');
 
-  eventEmitter.on("stickRun", push);
+  // const push = (isMoving: boolean) => {
+  //   console.log('stickRun push', isMoving);
+  //   eventStream.push(JSON.stringify(isMoving));
+  // };
 
-  const interval = setInterval(async () => {
-    // 杖の振り判定の取得
-    const isMoving: boolean = await getIsMoving();
-    eventEmitter.emit("stickRun", isMoving);
-  }, 100);
+  // eventEmitter.on('stickRun', push);
+  console.log('stickRun push', 'eventEmitter.on');
+
+  eventEmitter.on('stickRun', async ({ isMoving }) => {
+    console.log('stickRun push', isMoving);
+    await eventStream.push(JSON.stringify(isMoving));
+  });
+
+  // const interval = setInterval(async () => {
+  //   // 杖の振り判定の取得
+  //   const isMoving: boolean = await getIsMoving();
+  //   eventEmitter.emit('stickRun', isMoving);
+  //   console.log('stickRun', isMoving);
+  // }, 100);
 
   eventStream.onClosed(async () => {
-    clearInterval(interval);
-    eventEmitter.off("stickRun", push);
+    // eventEmitter.off('stickRun', push);
     await eventStream.close();
   });
+  return eventStream.send();
 });
